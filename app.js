d27f8d1aad6fdde1051fe416e42d35f21f7971e2
phamin2001 <amin.pahlavani@gmail.com>
2020-06-30 23:07:33 -0600

    Github Finder [Part 5] - Fetch & Display Repos

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ searchUser.addEventListener('keyup', e => {
       } else {
         // Show profiles
         ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
       }
     });
   } else {
--- github.js
+++ github.js
@@ -2,6 +2,8 @@ class GitHub {
   constructor() {
     this.client_id = '7acf005957020c2aecf2';
     this.client_secret = 'eb33e4a9ba2f57fa7e8ac003381e5ca91ef058f9';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   // Get User
@@ -10,10 +12,16 @@ class GitHub {
       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
 
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_count}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile // profile: profile
+      profile, // profile: profile
+      repos
     };
   }
 }

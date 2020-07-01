ecaff27d5eabfdb371f950e2d860f81a421ffcc4
phamin2001 <amin.pahlavani@gmail.com>
2020-06-30 23:09:48 -0600

    commit

--- ui.js
+++ ui.js
@@ -44,9 +44,9 @@ class UI {
               <a href="${repo.html_url}" target="_blank">${repo.name}</a>
             </div>
             <div class="col-md-6">
-            <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
-            <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
-            <span class="badge badge-success">Forks: ${repo.forks_count}</span>
+              <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+              <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+              <span class="badge badge-success">Forks: ${repo.forks_count}</span>
             </div>
           </div>
         </div>
